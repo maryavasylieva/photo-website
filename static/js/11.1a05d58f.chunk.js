f2e1d8384370ee7044dc08fbde5693010e0f80e3
maryavasylieva <miheta.laurelin@gmail.com>
2021-07-08 23:44:37 +0300

    Updates

--- static/js/11.1a05d58f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpphoto-website"]=this["webpackJsonpphoto-website"]||[]).push([[11],{283:function(t,e,s){"use strict";s.r(e);s(0);var n=s(1);e.default=function(){return Object(n.jsx)("h1",{children:"Contact Page"})}}}]);
-//# sourceMappingURL=11.1a05d58f.chunk.js.map
