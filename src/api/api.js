094d2cd68d89165d969f9fc79b9a37a5774c5be4
maryavasylieva <miheta.laurelin@gmail.com>
2021-09-10 17:13:36 +0300

    fix: fixed gitignore

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = process.env.PHOTO_APP_BASE_URL;
+// axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 export const getPhoto = async (quantity) =>
   await axios
